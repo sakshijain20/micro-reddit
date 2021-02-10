3daca1fb9f2307c70c77f9630e87559de866b977
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-02-10 14:54:17 +0530

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+	 has_many :posts, dependent: :destroy
+     has_many :comments, dependent: :destroy
+
 	validates :username ,presence: true
 	validates :password ,presence: true, length: { minimum: 8 }
 	validates :email ,presence: true
