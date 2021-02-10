8efddf58a84ebcd847a4d7c0af74fb86b0372108
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-02-10 14:34:45 +0530

    migrated db after post model creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_085003) do
+ActiveRecord::Schema.define(version: 2021_02_10_090322) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.text "body"
+    t.integer "readtime"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_02_10_085003) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
