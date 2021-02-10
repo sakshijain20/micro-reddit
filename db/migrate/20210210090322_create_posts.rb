9092a07739e5530d50a95ce48873b7c7cc96e6a8
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-02-10 14:33:46 +0530

    added post model

---
+++ db/migrate/20210210090322_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.references :user, null: false, foreign_key: true
+      t.text :body
+      t.integer :readtime
+
+      t.timestamps
+    end
+  end
+end
